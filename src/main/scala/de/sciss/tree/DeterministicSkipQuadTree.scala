705987192f8f959a54bfc6d63b0ac737ef414d8d
Sciss <contact@sciss.de>
2011-06-06 19:25:52 +0100

    factoring out TotalOrder trait, adding TotalOrder.max

--- src/main/scala/de/sciss/tree/DeterministicSkipQuadTree.scala
+++ src/main/scala/de/sciss/tree/DeterministicSkipQuadTree.scala
@@ -43,35 +43,46 @@ object DeterministicSkipQuadTree {
       t
    }
 
+//   sealed trait TreeNode
    sealed trait Q[ +V ]
 
+//   final case class QHemi[ V ](/*  parent: QNode[ V ], */ west: Q[ V ], east: Q[ V ]) extends TreeNode
    case object QEmpty extends Q[ Nothing ]
 
    final case class QLeaf[ V ]( point: Point, value: V ) extends Q[ V ]
 
+   type InOrder = TotalOrder[ Unit ]
+
    object T {
-      def apply[ V ]( quad: Quad ) = new T[ V ]( new QNode[ V ]( quad, None )() )
+      def apply[ V ]( quad: Quad ) = {
+         val inOrder = TotalOrder.empty[ Unit ]
+         val north   = inOrder.append( () )
+         val south   = north.insertAfter( () )
+         val root    = new QNode[ V ]( quad, None, north, south )()
+         new T[ V ]( inOrder, root )
+      }
    }
-   final class T[ V ]( val head: QNode[ V ]) {
+   final class T[ V ] private( inOrder: TotalOrder[ Unit ], val head: QNode[ V ]) {
       private var tailVar: QNode[ V ] = head
+//      private val inOrder = TotalOrder.empty
 
       def tail: QNode[ V ] = tailVar
 
       def insert( point: Point, value: V ) {
          val qpred   = tailVar.insertStep( point, value )
-         if( qpred.isEmpty || !flipCoin ) return
-         val hq      = head.quad
-         val qidx    = quadIdx( hq, point )
-         val l       = QLeaf( point, value )
-         do {
-            val quads      = new Array[ Q[ V ]]( 4 )
-            quads( qidx )  = l
-            tailVar        = new QNode[ V ]( hq, Some( tailVar ))( quads )
-         } while( flipCoin )
+//         if( qpred.isEmpty || !flipCoin ) return
+//         val hq      = head.quad
+//         val qidx    = quadIdx( hq, point )
+//         val l       = QLeaf( point, value )
+//         do {
+//            val quads      = new Array[ Q[ V ]]( 4 )
+//            quads( qidx )  = l
+//            tailVar        = new QNode[ V ]( hq, Some( tailVar ))( quads )
+//         } while( flipCoin )
       }
    }
 
-   def flipCoin : Boolean = util.Random.nextBoolean()
+//   def flipCoin : Boolean = util.Random.nextBoolean()
 
    /**
     * Determines the quadrant index of a point `a` in a square `p` defined
@@ -139,8 +150,8 @@ object DeterministicSkipQuadTree {
       }
    }
 
-   final case class QNode[ V ]( quad: Quad, pred: Option[ QNode[ V ]])( quads: Array[ Q[ V ]] = new Array[ Q[ V ]]( 4 ))
-   extends Q[ V ] /* with MaybeQNode[ V ] */ {
+   final case class QNode[ V ]( quad: Quad, pred: Option[ QNode[ V ]], north: InOrder, south: InOrder )( quads: Array[ Q[ V ]] = new Array[ Q[ V ]]( 4 ))
+   extends Q[ V ] /* with TreeNode */ /* with MaybeQNode[ V ] */ {
       // fix null squares
       {
          var i = 0; while( i < 4 ) {
@@ -148,24 +159,29 @@ object DeterministicSkipQuadTree {
          i += 1 }
       }
 
-      def child( idx: Int ) : Q[ V ] = quads( idx )
+//      def child( idx: Int ) : Q[ V ] = quads( idx )
+
+      private def createOrders( qidx: Int ) : (InOrder, InOrder) = {
+         val parent = if( qidx < 2 ) north else south
+         (parent.insertBefore( () ), parent.insertAfter( () ))
+      }
 
       def insertStep( point: Point, value: V ) : Option[ QNode[ V ]] = {
          val qidx = quadIdx( quad, point )
 //         require( qidx >= 0, point.toString + " lies outside of root square " + quad )
          quads( qidx ) match {
             case QEmpty =>
-               if( pred.isEmpty || (pred.flatMap( _.insertStep( point, value )).nonEmpty && flipCoin) ) {
+               if( pred.isEmpty /* || (pred.flatMap( _.insertStep( point, value )).nonEmpty && flipCoin) */ ) {
                   quads( qidx ) = QLeaf( point, value )
                   Some( this )
                } else None
 
-            case t @ QNode( tq, tpred ) =>
+            case t @ QNode( tq, tpred, _, _ ) =>
                if( tq.contains( point )) {
                   t.insertStep( point, value )
                } else {
-                  val qpred = pred.flatMap( _.insertStep( point, value ))
-                  if( pred.isEmpty || (qpred.nonEmpty && flipCoin) ) {
+//                  val qpred = pred.flatMap( _.insertStep( point, value ))
+                  if( pred.isEmpty /* || (qpred.nonEmpty && flipCoin) */ ) {
                      val te      = tq.extent
                      val iq      = gisqr( qidx, tq.cx - te, tq.cy - te, te << 1, point )
                      val iquads  = new Array[ Q[ V ]]( 4 )
@@ -173,22 +189,24 @@ object DeterministicSkipQuadTree {
                      iquads( tidx ) = t
                      val pidx    = quadIdx( iq, point )
                      iquads( pidx ) = QLeaf( point, value )
-                     val q       = QNode[ V ]( iq, qpred )( iquads )
+                     val (north, south) = createOrders( qidx )
+                     val q       = QNode[ V ]( iq, None /* qpred */, north, south )( iquads )
                      quads( qidx ) = q
                      Some( q )
                   } else None
                }
 
             case l @ QLeaf( point2, value2 ) =>
-               val qpred   = pred.flatMap( _.insertStep( point, value ))
-               if( pred.isEmpty || (qpred.nonEmpty && flipCoin) ) {
+//               val qpred   = pred.flatMap( _.insertStep( point, value ))
+               if( pred.isEmpty /* || (qpred.nonEmpty && flipCoin) */ ) {
                   val iq      = gisqr( qidx, point2.x, point2.y, 1, point )
                   val iquads  = new Array[ Q[ V ]]( 4 )
                   val lidx    = quadIdx( iq, point2 )
                   iquads( lidx ) = l
                   val pidx    = quadIdx( iq, point )
                   iquads( pidx ) = QLeaf( point, value )
-                  val q       = QNode[ V ]( iq, qpred )( iquads )
+                  val (north, south) = createOrders( qidx )
+                  val q       = QNode[ V ]( iq, None /* qpred */, north, south )( iquads )
                   quads( qidx ) = q
                   Some( q )
                } else None
--- src/main/scala/de/sciss/tree/TotalOrder.scala
+++ src/main/scala/de/sciss/tree/TotalOrder.scala
@@ -61,9 +61,23 @@ object TotalOrder extends SeqFactory[ TotalOrder ] {
 //      def compare( x: TotalOrder[ V ], y: TotalOrder[ V ]) = x.compare( y )
 //   }
 
+   /**
+    * Returns a single element order corresponding to the tag ceiling. This
+    * can be used for comparison (using its `Ordered` trait).
+    *
+    * Note: you can not add elements before or after this one.
+    */
+   def max[ A ] : TotalOrder[ A ] = new Impl[ A ]( new Size ) {
+      tag = Int.MaxValue
+      override def append( elem: A ) : T = unsupportedOp
+      override def insertAfter( elem: A ) : T = unsupportedOp
+      override def insertBefore( elem: A ) : T = unsupportedOp
+      def unsupportedOp = error( "Operation not permitted" )
+   }
+
    // ---- GenericCompanion ----
   def newBuilder[ A ] = new Builder[ A, TotalOrder[ A ]] {
-      def emptyList() = new TotalOrder[ A ]( new Size )
+      def emptyList() : TotalOrder[ A ] = new Impl[ A ]( new Size )
       var head = emptyList()
       var tail = head
 
@@ -94,66 +108,203 @@ object TotalOrder extends SeqFactory[ TotalOrder ] {
       def value : Int = v
       def inc { v += 1 }
    }
-}
 
-object TotalOrderTest extends App {
-   val to    = TotalOrder[ Int ]()
-   val rnd   = new util.Random( 0 )
-   val n     = 3042 // 3041
-   var pred  = to.append( 0 )
-   for( i <- 1 until n ) {
-      if( i == n - 1 ) {
-         println( "last" )
-//         TotalOrder.flonky = true
+   private class Impl[ A ]( protected val totalSize: TotalOrder.Size ) // (_t: Int)
+   extends TotalOrder[ A ] {
+//      private type T = TotalOrder[ A ]
+
+      next = this
+      protected var tag: Int = 0 // _t
+
+      def this( sz: TotalOrder.Size, elem: A, prev: TotalOrder[ A ], next: TotalOrder[ A ]) {
+         this( sz )
+         sz.inc
+         this.elem      = elem
+         this.prev      = prev
+         if( prev != null ) {
+            require( prev.nonEmpty && (prev.next eq next) )
+            prev.next = this
+         } else {
+            require( next.isHead )
+         }
+         this.next      = next
+         next.prev      = this
       }
-      pred   = if( rnd.nextBoolean() ) {
-         pred.insertAfter( i )
-      } else {
-         pred.insertBefore( i )
+
+   //   type Rec = Record[ T ]
+   //
+   //   private var base : Rec = null
+
+      def isHead : Boolean = prev == null
+      def isLast : Boolean = next.isEmpty
+
+      /**
+       * Compares the positions of x and y in the sequence
+      */
+      def compare( that: T ) : Int = tag compare that.tag
+
+      /**
+       * Appends an element to the end of the sequence, and returns
+       * that new sequence tail. Note that this operation takes O(n).
+       *
+       * @param   elem  the element to append
+       * @return  the total order entry corresponding to the newly appended element
+       */
+      def append( elem: A ) : T = {
+         if( isEmpty ) {
+            this.elem   = elem
+            tag         = Int.MaxValue >> 1
+            val empty : T = new Impl( totalSize )
+            totalSize.inc
+            next        = empty
+            empty.prev  = this
+            this
+         } else {
+            lastNode.insertAfter( elem )
+         }
+      }
+
+      private def lastNode : T = {
+         var res = next
+         while( !res.isEmpty ) res = res.next
+         res.prev
+      }
+
+      def insertAfter( elem: A ) : T = {
+         val rec     = new Impl( totalSize, elem, this, next )
+         val nextTag = if( rec.next.isEmpty ) Int.MaxValue else rec.next.tag
+         rec.tag     = tag + ((nextTag - tag + 1) >> 1)
+         if( rec.tag == nextTag ) rec.relabel
+         rec
+      }
+
+      def insertBefore( elem: A ) : T = {
+         if( isHead ) {
+            // to maintain references to the 'head' of the list,
+            // in the case when an element is inserted at the
+            // head of the list, we instead change this entry's
+            // elem and tag, and a new successor is inserted
+            // after this head
+            val rec     = new Impl( totalSize, this.elem, this, next )
+            this.elem   = elem
+            rec.tag     = tag
+            tag         = (tag + 1) >> 1
+            if( tag == rec.tag ) this.relabel
+            this
+         } else {
+            val rec     = new Impl( totalSize, elem, prev, this )
+            val prevTag = rec.prev.tag
+            rec.tag     = prevTag + ((tag - prevTag + 1) >> 1)
+            if( rec.tag == tag ) rec.relabel
+            rec
+         }
+      }
+
+//      /**
+//       * Relabels from a this entry to clean up collisions with
+//       * its successors' tags.
+//       *
+//       * Naive implementation.
+//       */
+//      protected def relabelSIMPLE {
+//         var base = this.tag // 0
+//         val inc  = (Int.MaxValue - base) / size
+//         require( inc > 0, "label overflow" )
+//   //println( "relabel (" + inc + ")" )
+//         var entry = this
+//         while( entry.nonEmpty ) {
+//            entry.tag = base
+//            base += inc
+//            entry = entry.next
+//         }
+//      }
+
+      protected def relabel {
+         var base       = tag
+         var mask       = -1
+         var thresh     = 1.0
+         var first : T  = this
+         var last : T   = this
+         var num        = 1
+   //      val mul     = 2/((2*len(self))**(1/30.))
+         val mul        = 2 / math.pow( totalSize.value << 1, 1/30.0 )
+   //println( "relabel" )
+         do {
+   //println( "   -mask " + -mask )
+            while( !first.isHead && ((first.prev.tag & mask) == base) ) {
+               first = first.prev
+               num  += 1
+            }
+            while( !last.isLast && ((last.next.tag & mask) == base) ) {
+               last = last.next
+               num += 1
+            }
+   //         val inc = (mask + 1) / num
+            val inc = -mask / num
+            if( inc >= thresh ) {   // found rebalanceable range
+               var item = first
+   //            while( !(item eq last) ) {
+               // Note: this was probably a bug in Eppstein's code
+               // -- it ran for one iteration less which made
+               // the test suite fail for very dense tags. it
+               // seems now it is correct with the inclusion
+               // of last in the tag updating.
+               var cnt = 0; while( cnt < num ) {
+                  item.tag   = base
+                  item       = item.next
+                  base      += inc
+                  cnt += 1
+               }
+               return
+            }
+   //         mask     = (mask << 1) + 1    // expand to next power of two
+            mask   <<= 1      // next coarse step
+            base    &= mask
+            thresh  *= mul
+         } while( mask != 0 )
+         throw new RuntimeException( "label overflow" )
+      }
+
+      def validateToEnd {
+         var prevTag = tag
+         var entry   = next
+         while( entry.nonEmpty ) {
+            assert( entry.tag > prevTag, "elem " + entry.elem + " at index " + indexOf( entry.elem ) + " has tag " + entry.tag +
+               ", while previous elem has tag " + prevTag )
+            prevTag  = entry.tag
+            entry    = entry.next
+         }
+      }
+
+      def tagList : List[ Int ] = {
+         val b       = List.newBuilder[ Int ]
+         var entry : T = this
+         while( entry.nonEmpty ) {
+            b += entry.tag
+            entry    = entry.next
+         }
+         b.result()
       }
    }
-   to.validateToEnd
-   println( "OK." )
 }
 
-final class TotalOrder[ A ] private ( protected val totalSize: TotalOrder.Size ) // (_t: Int)
+sealed trait TotalOrder[ A ]
 extends MLinearSeq[ A ]
 with GenericTraversableTemplate[ A, TotalOrder ]
 with DoubleLinkedListLike[ A, TotalOrder[ A ]] with Ordered[ TotalOrder[ A ]] {
+   def isHead : Boolean
+   def isLast : Boolean
+
    private type T = TotalOrder[ A ]
-
-   next = this
-   protected var tag: Int = 0 // _t
-
-   private def this( sz: TotalOrder.Size, elem: A, prev: TotalOrder[ A ], next: TotalOrder[ A ]) {
-      this( sz )
-      sz.inc
-      this.elem      = elem
-      this.prev      = prev
-      if( prev != null ) {
-         require( prev.nonEmpty && (prev.next eq next) )
-         prev.next = this
-      } else {
-         require( next.isHead )
-      }
-      this.next      = next
-      next.prev      = this
-   }
 
    // ---- GenericTraversableTemplate ----
    override def companion: GenericCompanion[ TotalOrder ] = TotalOrder
 
-//   type Rec = Record[ T ]
-//
-//   private var base : Rec = null
-
-   def isHead : Boolean = prev == null
-   def isLast : Boolean = next.isEmpty
 
    /**
     * Compares the positions of x and y in the sequence
    */
-   def compare( that: T ) : Int = tag compare that.tag
+   def compare( that: T ) : Int
 
    /**
     * Appends an element to the end of the sequence, and returns
@@ -162,80 +313,33 @@ with DoubleLinkedListLike[ A, TotalOrder[ A ]] with Ordered[ TotalOrder[ A ]] {
     * @param   elem  the element to append
     * @return  the total order entry corresponding to the newly appended element
     */
-   def append( elem: A ) : T = {
-      if( isEmpty ) {
-         this.elem   = elem
-         tag         = Int.MaxValue >> 1
-         val empty   = new T( totalSize )
-         totalSize.inc
-         next        = empty
-         empty.prev  = this
-         this
-      } else {
-         lastNode.insertAfter( elem )
-      }
-   }
-
-   private def lastNode : T = {
-      var res = next
-      while( !res.isEmpty ) res = res.next
-      res.prev
-   }
+   def append( elem: A ) : T
 
    /**
     * Inserts a new element after this node
     */
-   def insertAfter( elem: A ) : T = {
-      val rec     = new T( totalSize, elem, this, next )
-      val nextTag = if( rec.next.isEmpty ) Int.MaxValue else rec.next.tag
-      rec.tag     = tag + ((nextTag - tag + 1) >> 1)
-      if( rec.tag == nextTag ) rec.relabel
-      rec
-   }
+   def insertAfter( elem: A ) : T
 
    /**
     * Inserts a new element before this node
     */
-   def insertBefore( elem: A ) : T = {
-      if( isHead ) {
-         // to maintain references to the 'head' of the list,
-         // in the case when an element is inserted at the
-         // head of the list, we instead change this entry's
-         // elem and tag, and a new successor is inserted
-         // after this head
-         val rec     = new T( totalSize, this.elem, this, next )
-         this.elem   = elem
-         rec.tag     = tag
-         tag         = (tag + 1) >> 1
-         if( tag == rec.tag ) this.relabel
-         this
-      } else {
-         val rec     = new T( totalSize, elem, prev, this )
-         val prevTag = rec.prev.tag
-         rec.tag     = prevTag + ((tag - prevTag + 1) >> 1)
-         if( rec.tag == tag ) rec.relabel
-         rec
-      }
-   }
+   def insertBefore( elem: A ) : T
 
    /**
-    * Relabels from a this entry to clean up collisions with
-    * its successors' tags.
-    *
-    * Naive implementation.
+    * Debugging method: Validates that the list from this entry
+    * to the end has monotonically increasing
+    * tags. Throws an assertion error if the
+    * validation fails.
     */
-   protected def relabelSIMPLE {
-      var base = this.tag // 0
-      val inc  = (Int.MaxValue - base) / size
-      require( inc > 0, "label overflow" )
-//println( "relabel (" + inc + ")" )
-      var entry = this
-      while( entry.nonEmpty ) {
-         entry.tag = base
-         base += inc
-         entry = entry.next
-      }
-   }
+   def validateToEnd : Unit
+
+   /**
+    * Debugging method: Returns a list of the tags
+    * from this entry to the end of the list
+    */
+   def tagList : List[ Int ]
+
+   protected def totalSize : TotalOrder.Size
 
    /**
     * Relabels from a this entry to clean up collisions with
@@ -256,80 +360,28 @@ with DoubleLinkedListLike[ A, TotalOrder[ A ]] with Ordered[ TotalOrder[ A ]] {
     * multiplier dynamically to allow it to be as large as possible
     * without producing integer overflows."
     */
-   protected def relabel {
-      var base    = tag
-      var mask    = -1
-      var thresh  = 1.0
-      var first   = this
-      var last    = this
-      var num     = 1
-//      val mul     = 2/((2*len(self))**(1/30.))
-      val mul     = 2 / math.pow( totalSize.value << 1, 1/30.0 )
-//println( "relabel" )
-      do {
-//println( "   -mask " + -mask )
-         while( !first.isHead && ((first.prev.tag & mask) == base) ) {
-            first = first.prev
-            num  += 1
-         }
-         while( !last.isLast && ((last.next.tag & mask) == base) ) {
-            last = last.next
-            num += 1
-         }
-//         val inc = (mask + 1) / num
-         val inc = -mask / num
-         if( inc >= thresh ) {   // found rebalanceable range
-            var item = first
-//            while( !(item eq last) ) {
-            // Note: this was probably a bug in Eppstein's code
-            // -- it ran for one iteration less which made
-            // the test suite fail for very dense tags. it
-            // seems now it is correct with the inclusion
-            // of last in the tag updating.
-            var cnt = 0; while( cnt < num ) {
-               item.tag   = base
-               item       = item.next
-               base      += inc
-               cnt += 1
-            }
-            return
-         }
-//         mask     = (mask << 1) + 1    // expand to next power of two
-         mask   <<= 1      // next coarse step
-         base    &= mask
-         thresh  *= mul
-      } while( mask != 0 )
-      throw new RuntimeException( "label overflow" )
-   }
+   protected def relabel : Unit
 
-   /**
-    * Debugging method: Validates that the list from this entry
-    * to the end has monotonically increasing
-    * tags. Throws an assertion error if the
-    * validation fails.
-    */
-   def validateToEnd {
-      var prevTag = tag
-      var entry   = next
-      while( entry.nonEmpty ) {
-         assert( entry.tag > prevTag, "elem " + entry.elem + " at index " + indexOf( entry.elem ) + " has tag " + entry.tag +
-            ", while previous elem has tag " + prevTag )
-         prevTag  = entry.tag
-         entry    = entry.next
+   protected def tag: Int
+   protected def tag_=( i: Int ) : Unit
+}
+
+object TotalOrderTest extends App {
+   val to    = TotalOrder[ Int ]()
+   val rnd   = new util.Random( 0 )
+   val n     = 3042 // 3041
+   var pred  = to.append( 0 )
+   for( i <- 1 until n ) {
+      if( i == n - 1 ) {
+         println( "last" )
+//         TotalOrder.flonky = true
+      }
+      pred   = if( rnd.nextBoolean() ) {
+         pred.insertAfter( i )
+      } else {
+         pred.insertBefore( i )
       }
    }
-
-   /**
-    * Debugging method: Returns a list of the tags
-    * from this entry to the end of the list
-    */
-   def tagList : List[ Int ] = {
-      val b       = List.newBuilder[ Int ]
-      var entry   = this
-      while( entry.nonEmpty ) {
-         b += entry.tag
-         entry    = entry.next
-      }
-      b.result()
-   }
+   to.validateToEnd
+   println( "OK." )
 }
