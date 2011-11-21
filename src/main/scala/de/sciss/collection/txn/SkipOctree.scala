6a7689ff58fa8f8884564283afde52c094599e00
Sciss <contact@sciss.de>
2011-11-21 14:31:17 +0000

    continue work on txn.DeterministicSkipOctree

---
+++ src/main/scala/de/sciss/collection/txn/SkipOctree.scala
@@ -0,0 +1,126 @@
+/*
+ *  SkipOctree.scala
+ *  (TreeTests)
+ *
+ *  Copyright (c) 2011 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either
+ *  version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License (gpl.txt) along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.collection
+package txn
+
+import de.sciss.collection.geom.{Space, DistanceMeasure, QueryShape}
+import de.sciss.lucrestm.Sys
+
+/**
+ * A `SkipOctree` is a multi-dimensional data structure that
+ * maps coordinates to values. It extends the interface
+ * of scala's mutable `Map` and adds further operations such
+ * as range requires and nearest neighbour search.
+ */
+trait SkipOctree[ S <: Sys[ S ], D <: Space[ D ], @specialized( Int, Long ) A ] {
+   def space: D
+
+//   def headTree: QNode
+//   def lastTree: QNode
+   def pointView : A => D#Point
+
+   def hyperCube : D#HyperCube
+
+   def numLevels( implicit tx: S#Tx ) : Int
+
+   /**
+    * The number of orthants in each hyperCube. This is equal
+    * to `1 << numDimensions` and gives the upper bound
+    * of the index to `QNode.child()`.
+    */
+   def numOrthants : Int
+
+   def get( point: D#Point )( implicit tx: S#Tx ) : Option[ A ]
+   def isDefinedAt( point: D#Point )( implicit tx: S#Tx ) : Boolean
+
+   def removeAt( point: D#Point )( implicit tx: S#Tx ) : Option[ A ]
+
+   /**
+    * Adds an element to the tree
+    *
+    * @return  true if the element is new in the tree. If a previous entry with the
+    *          same point view is overwritten, this is true if the elements were
+    *          equal, false otherwise
+    */
+   def add( elem: A )( implicit tx: S#Tx ) : Boolean
+
+   /**
+    * Adds an element to the tree
+    *
+    * @return  the old element stored for the same point view, if it existed
+    */
+   def update( elem: A )( implicit tx: S#Tx ) : Option[ A ]
+
+   def rangeQuery[ @specialized( Long ) Area ]( qs: QueryShape[ Area, D ])( implicit tx: S#Tx ) : Iterator[ A ]
+
+   /**
+    * Reports the nearest neighbor entry with respect to
+    * a given point.
+    *
+    * Note: There is a potential numeric overflow if the
+    * squared distance of the query point towards the
+    * furthest corner of the tree's root hyper-cube exceeds 63 bits.
+    * For a root `Square( 0x40000000, 0x40000000, 0x40000000 )`, this
+    * happens for example for any point going more towards north-west
+    * than `Point2DLike( -1572067139, -1572067139 )`.
+    *
+    * @param   point the point of which the nearest neighbor is to be found
+    * @param   a threshold which is an acceptable abortion criterion. I.e.,
+    *    if a point is found whose distance is smaller or equal to this
+    *    value, the search is immediately terminated and that entry is returned
+    *
+    * @throws  NoSuchElementException  if the tree is empty
+    */
+   def nearestNeighbor[ @specialized( Long ) M ]( point: D#Point, metric: DistanceMeasure[ M, D ])
+                                                ( implicit tx: S#Tx ) : A
+
+   def nearestNeighborOption[ @specialized( Long ) M ]( point: D#Point, metric: DistanceMeasure[ M, D ])
+                                                      ( implicit tx: S#Tx ) : Option[ A ]
+
+   /**
+    * An `Iterator` which iterates over the points stored
+    * in the octree, using an in-order traversal directed
+    * by the orthant indices of the nodes of the tree
+    */
+   def iterator( implicit tx: S#Tx ) : Iterator[ A ]
+
+//   /* sealed */ trait Q
+//   trait QEmpty extends Q
+//   sealed trait QNonEmpty extends Q
+//   trait QLeaf extends QNonEmpty {
+//      def value: A
+//   }
+//   trait QNode extends QNonEmpty {
+//      def hyperCube: D#HyperCube
+//      def child( idx: Int )( implicit tx: S#Tx ) : Q
+//      def prevOption( implicit tx: S#Tx ): Option[ QNode ]
+//      def nextOption( implicit tx: S#Tx ): Option[ QNode ]
+//
+//      // XXX todo: how can we make this implementation private?
+//      /* protected[SkipOctree] */ def prev( implicit tx: S#Tx ): QNode
+//      /* protected[SkipOctree] */ def next( implicit tx: S#Tx ): QNode
+//   }
+}
