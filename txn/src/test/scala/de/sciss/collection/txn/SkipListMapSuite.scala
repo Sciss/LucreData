7fbd7c9a4d29ba1e06d25eed8d2d4565b6e3d084
Sciss <contact@sciss.de>
2012-06-09 00:36:27 +0100

    ...

--- txn/src/test/scala/de/sciss/collection/txn/SkipListMapSuite.scala
+++ txn/src/test/scala/de/sciss/collection/txn/SkipListMapSuite.scala
@@ -31,35 +31,46 @@ class SkipListMapSuite extends FeatureSpec with GivenWhenThen {
       info( "Several mass operations on the structure" )
       info( "are tried and expected behaviour verified" )
 
-      scenarioWithTime( "Consistency", "Consistency is verified on a randomly filled structure" ) {
-         val system = InMemory()
-         def atomic[ A ]( fun: S#Tx => A ) : A = system.step( fun )
+      val system = InMemory()
+      def atomic[ A ]( fun: S#Tx => A ) : A = system.step( fun )
 
-         val map = atomic { implicit tx => SkipList.Map.empty[ S, Int, Int ]}
+      val map = atomic { implicit tx => SkipList.Map.empty[ S, Int, Int ]}
 
+      scenarioWithTime( "empty", "Consistency is verified on an empty map" ) {
          def onEmptyList() {
-            when( "the (size, isEmpty, nonEmpty) are queried" )
-            val szTup = atomic { implicit tx => (map.size, map.isEmpty, map.nonEmpty) }
-            then( "they should be (0, true, false)" )
-            assert( szTup == (0, true, false), "found " + szTup )
-            when( "the floor or ceil of a number if queried" )
-            val flcl = atomic { implicit tx => (map.floor( 42 ), map.ceil( 42 ))}
-            then( "they should be undefined" )
-            assert( flcl == (None, None), "found " + flcl )
+            when( "the (size, isEmpty, nonEmpty, height) are queried" )
+            val szTup = atomic { implicit tx => (map.size, map.isEmpty, map.nonEmpty, map.height) }
+            then( "they should be (0, true, false, 0)" )
+            assert( szTup == (0, true, false, 0), "found " + szTup )
+
+            when( "the (floor, ceil, contains) of a number if queried" )
+            val flcl = atomic { implicit tx => (map.floor( 42 ), map.ceil( 42 ), map.contains( 42 ))}
+            then( "they should be (None, None, false)" )
+            assert( flcl == (None, None, false), "found " + flcl )
+
+            when( "the iterator and sequence methods are called" )
+            val colls = atomic { implicit tx => (map.iterator.isEmpty, map.toSeq.isEmpty, map.toSet.isEmpty, map.toIndexedSeq.isEmpty) }
+            then( "they should all return empty collections" )
+            assert( colls == (true, true, true, true), "found " + colls )
          }
 
          onEmptyList()
+      }
 
+      scenarioWithTime( "filled", "Consistency is verified on a randomly filled map" ) {
          val seq = atomic { implicit tx =>
             val _s   = IIdxSeq.tabulate( N )( n => rnd.nextInt() -> n )
             _s.foreach( map += _ )
             _s
          }
 
-         when( "the size is queried" )
-         val sz = atomic { implicit tx => map.size }
-         then( "it should be N (" + N + ")" )
-         assert( sz == N, "found " + sz )
+         val maxHeight = math.ceil( math.log( N + 1 ) / math.log( map.minGap )).toInt
+
+         when( "the (size, height) is queried" )
+         val (sz, h) = atomic { implicit tx => (map.size, map.height) }
+         then( "it should be (N, <=log_minGap(N+1)) (" + (N -> maxHeight) + ")" )
+         assert( sz == N, "found size " + sz )
+         assert( h <= maxHeight, "found height " + h )
       }
    }
 }
