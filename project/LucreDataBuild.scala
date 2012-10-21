f24e50731b382f1f0605bf2c559736319b81a684
Sciss <contact@sciss.de>
2012-10-21 14:37:27 +0200

    make it 1.3.0

--- project/LucreDataBuild.scala
+++ project/LucreDataBuild.scala
@@ -13,7 +13,7 @@ object LucreDataBuild extends Build {
       base      = file( "core" ),
       settings     = Project.defaultSettings ++ Seq(
          libraryDependencies ++= Seq(  
-            "de.sciss" %% "lucrestm" % "1.3.0-SNAPSHOT"
+            "de.sciss" %% "lucrestm" % "1.3.0"
 //            "org.scalatest" %% "scalatest" % "1.7.2" % "test"
          )
       )
