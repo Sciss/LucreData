51673d9a1adb16deed5ae15824727ac98f0bc6a0
Sciss <contact@sciss.de>
2012-10-28 23:19:33 +0100

    use stm snapshot

--- project/LucreDataBuild.scala
+++ project/LucreDataBuild.scala
@@ -13,7 +13,7 @@ object LucreDataBuild extends Build {
       base      = file( "core" ),
       settings     = Project.defaultSettings ++ Seq(
          libraryDependencies ++= Seq(  
-            "de.sciss" %% "lucrestm" % "1.3.0"
+            "de.sciss" %% "lucrestm" % "1.4.0-SNAPSHOT"
 //            "org.scalatest" %% "scalatest" % "1.7.2" % "test"
          )
       )
