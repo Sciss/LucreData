711cb5fa381d864e295bba69651983099d454bb5
Sciss <contact@sciss.de>
2013-04-18 23:12:58 +0200

    damn it

--- core/src/test/scala/de/sciss/lucre/data/NNPerf.scala
+++ core/src/test/scala/de/sciss/lucre/data/NNPerf.scala
@@ -11,15 +11,20 @@ object NNPerf extends App {
   val i   = InMemory()
   var j   = 32
   val r   = new util.Random(0L)
+  val down = 1
   while (j <= 262144) {
     i.step { implicit tx =>
       val cube  = IntSquare(j >> 1, j >> 1, j >> 1)
       val tree  = SkipOctree.empty[S, D, IntPoint2D](cube)
-      //val ins   = (0 until j by 1).map { i => IntPoint2D(i, j >> 1) }
-      val ins   = Vector.fill(j)(IntPoint2D(r.nextInt(j), r.nextInt(j)))
+//      val ins   = (0 until j by down).map { i => IntPoint2D(i, j >> 1) }
+//      val v     = IntPoint2D(0, j - 1)
+      val ins   = (0 until j by down).map { i => IntPoint2D(j >> 1, i) }
+      val v     = IntPoint2D(j - 1, 0)
+
+      //val ins   = Vector.fill(j)(IntPoint2D(r.nextInt(j), r.nextInt(j)))
       ins.foreach(tree += _)
       println(s"\nSize = $j")
-      tree.nearestNeighbor(IntPoint2D(0, j - 1), IntDistanceMeasure2D.euclideanSq)
+      tree.nearestNeighbor(v, IntDistanceMeasure2D.euclideanSq)
     }
 
     j <<= 1
